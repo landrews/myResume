c751591cf1085ca0e52f83fd7840417301180da1
Lance Andrews <ljandrews78@gmail.com>
2018-04-01 18:21:50 -0400

    adding GitHub section and contributions table; text and link changes

--- js/resume.js
+++ js/resume.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
     var year = new Date();
     $("#copyrightYear").html("<p>&copy; " + year.getFullYear() + " All rights reserved.</p>");
+
+    GitHubCalendar(".calender", "landrews", );
 });
