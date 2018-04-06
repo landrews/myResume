ddc2aef72a7689546e421363583cd5891e4750e7
Lance Andrews <ljandrews78@gmail.com>
2018-04-06 16:55:12 -0400

    adding updated resume; changed footer text; including React to skill set

--- js/resume.js
+++ js/resume.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var year = new Date();
-    $("#copyrightYear").html("<p>&copy; " + year.getFullYear() + " All rights reserved.</p>");
+    $("#copyrightYear").html('<p>&copy;' + year.getFullYear() + 'Built and maintained with &hearts; by <a href="https://github.com/landrews" target="_blank">@landrews</a>.</p>');
 
     GitHubCalendar(".calender", "landrews");
 });
