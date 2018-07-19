38a911c9b58a225b42cd5ca8b4a0ea506807cae8
Lance Andrews <ljandrews78@gmail.com>
2018-07-19 04:09:36 -0400

    removing angular project from this

--- vanillaResume/js/resume.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    var year = new Date();
-    $("#copyrightYear").html('<p>&copy;' + year.getFullYear() + ' | Built and maintained with &hearts; by <a href="https://github.com/landrews" target="_blank">@landrews</a>.</p>');
-
-    GitHubCalendar(".calender", "landrews");
-});
